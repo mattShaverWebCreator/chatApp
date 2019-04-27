4ed95b44d3479cec15900b599b6beb5f6e177986
mattShaverWebCreator <mattshaver@boostthegram.com>
2019-04-27 13:10:14 -0400

    Added state.

--- src/features/chat/Chat.js
+++ src/features/chat/Chat.js
@@ -3,4 +3,26 @@ import PropTypes from 'prop-types'
 
 class Chat extends Component {
 
+   constructor() {
+     super()
+     this.state = { listofmessages: [], messagetext: ''}
+   }
+
+   render() {
+     return (
+       <React.Fragment>
+         <ChatMessageView listofmessages={this.props.listofmessages} messagetext={this.props.messagetext}/>
+         <ChatInput listofmessages={this.props.listofmessages} messagetext={this.state.messagetext}/>
+       </React.Fragment>
+    )
+   }
+
+   const listofmessages = [
+     { username: "Joe Rogan", date: "1/11/1111", image: "default", text: "REPTILES RULE EVERYTHING AROUND ME" }
+   ]
+
+
+   }
+
+
 }
