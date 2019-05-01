a6c0e1ddd53ae5d8f4e8891c46d2fb8dab7db7ad
mattShaverWebCreator <mattshaver@boostthegram.com>
2019-05-01 13:49:39 -0400

    Added husky, lint-staged, prettier, material-ui.

--- src/features/chat/ChatMessageView.js
+++ src/features/chat/ChatMessageView.js
@@ -1,32 +1,25 @@
-import React from "react";
-import MessageInfoContainer from './MessageInfoContainer.js'
+import React from 'react';
+import MessageInfoContainer from './MessageInfoContainer.js';
 
-const ChatMessageView = (props) => {
-
+const ChatMessageView = props => {
   const messageinfocontainers = [];
-  const username = props.messages.username;
-  const date = props.messages.date;
   const messages = props.messages;
-  const userimage = props.messages.image;
 
-  console.log(messages)
+  console.log(messages);
 
-messages.forEach((message) => {
-      messageinfocontainers.push (
-        <MessageInfoContainer message={message} key={message.text}/>
-      );
-});
-
+  messages.forEach(message => {
+    messageinfocontainers.push(
+      <MessageInfoContainer message={message} key={message.text} />
+    );
+  });
 
   return (
-
-        <section className="chatmessageview">
-         <div className="chatmessageview__chatmessage">
-          {messageinfocontainers}
-         </div>
-        </section>
-      )
-
-}
+    <section className="chatmessageview">
+      <div className="chatmessageview__chatmessage">
+        {messageinfocontainers}
+      </div>
+    </section>
+  );
+};
 
 export default ChatMessageView;
